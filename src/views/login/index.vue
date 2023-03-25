e90457bc5e6922d01959028dd8dfaf368592a34c
luolei <920151816@qq.com>
2023-03-26 00:41:25 +0800

    登录页面

---
+++ src/views/login/index.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="common-layout">
+        <div class="box">
+            <div class="box_title">
+                <h1>车辆管理系统</h1>
+            </div>
+            <div class="box_form">
+                <div  class="box_login">
+                    <el-form :model="form" label-width="100px" >
+                        <el-form-item label="用户名" class="box_item">
+                            <el-input v-model="form.name" />
+                        </el-form-item>
+                        <el-form-item label="密码" class="box_item">
+                            <el-input type="password" v-model="form.password" />
+                        </el-form-item>
+                        <el-form-item>
+                            <el-button :loading="loading"  type="primary" @click="submitForm()">
+                                登录
+                            </el-button>
+                        </el-form-item>
+                    </el-form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            form: {
+                name: 'admin',
+                password: '123456'
+            },
+            loading:false
+        }
+    },
+    methods:{
+        submitForm(){
+            this.$message.success('成功')
+            // 发起请求
+            this.loading=true
+        }
+    }
+}
+</script>
+
+<style >
+.common-layout {
+    background: url('@/assets/background/desola-lanre-ologun-zYgV-NGZtlA-unsplash.jpg');
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background-size: 100% 100%;
+}
+
+.box {
+    min-width: 600px;
+    min-height: 260px;
+    background: rgba(255, 255, 255, 0.2);
+    width: 30vw;
+    height: 10vw;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    left: 50%;
+    top: 40%;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    border-radius: 25px;
+    backdrop-filter: blur(10px);
+}
+
+.box_title{
+    padding: 10px;
+}
+
+.box_form {
+    width: 100%;
+    height: 100%;
+    border-top-right-radius: 25px;
+    border-bottom-right-radius: 25px;
+    display: flex;
+    justify-content: center;
+}
+
+.box_login {
+    width: 50%;
+}
+
+.box_item .el-form-item__label{
+    color: black;
+}
+
+
+</style>
